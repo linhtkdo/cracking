4aef9dbe582e4b39e96ca5077a6762a7371c722f
Lynn Do <linhdo@umass.edu>
2016-06-30 21:15:26 +0700

    Update linked list.java

--- linked list.java
+++ linked list.java
@@ -1,1 +1,33 @@
 // Creating a linked list
+class Node {
+  Node next = null;
+  int data;
+  
+  public Node(int d) {
+    data = d;
+  }
+  
+  void appendToTail(int d) {
+    Node end = new Node(d);
+    Node n = this; // we access the linked list thru a ref to its head node
+    while (n.next != null) n = n.next;
+    n.next = end;
+  }
+  
+}
+
+// Deleting a node from a singly linked list
+Node deleteNode(Node head, int d) {
+  Node n = head;
+  
+  if (n.data == d) return head.next; // moved head
+  
+  while (n.next != null) { 
+    if (n.next.data == d) {
+      n.next = n.next.next;
+      return head; // head didn't change
+    }
+    n = n.next;
+  }
+  return head;
+}
