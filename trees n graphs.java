3a8429821d51cdf4c070327eb7acb51850cfb7a1
Lynn Do <linhdo@umass.edu>
2016-07-09 10:15:24 +0700

    Update and rename binary heap.java to trees n graphs.java

--- trees n graphs.java
+++ trees n graphs.java
@@ -54,3 +54,30 @@ public class MaxPQ<Key extends Comparable<Key>>
     {   Key t = pq[i]; pq[i] = pq[j]; pq[j] = t;    }
     
 }
+
+public class BST<Key extends Comparable<Key>, Value>
+{
+    private Node root;
+    
+    private class Node
+    {
+        private Key key;
+        private Value val;
+        private Node left, right;
+        public Node(Key key, Value val)
+        {
+            this.key = key;
+            this.val = val;
+        }
+    }
+    
+    // inorder traversal of a BST yields keys in ascending order
+    public void inorder(Node x)
+    {
+        if (x == null) return;
+        inorder(x.left);
+        visit(x);
+        inorder(x.right);
+    }
+}
+
