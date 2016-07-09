3545c3a8d285ab475284b2f20e55f931601d1be4
Lynn Do <linhdo@umass.edu>
2016-07-09 21:50:07 +0700

    Update trees n graphs.java

--- trees n graphs.java
+++ trees n graphs.java
@@ -81,3 +81,27 @@ public class BST<Key extends Comparable<Key>, Value>
     }
 }
 
+public class Graph
+{
+    // adj lists (using Bag data type)
+    private final int V;
+    private Bag<Integer>[] adj; 
+    
+    // create empty graph with V vertices
+    public Graph(int V)
+    {
+        this.V = V;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<Integer>();
+    }
+    
+    // add edge v-w
+    public void addEdge(int v, int w)
+    {
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+    
+    
+}
