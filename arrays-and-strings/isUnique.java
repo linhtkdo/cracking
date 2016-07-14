181b618e388938c941943cd7729a86d661bbc96c
Linh Do <linhdo@umass.edu>
2016-07-14 19:50:33 +0700

    Update isUnique.java

--- arrays-and-strings/isUnique.java
+++ arrays-and-strings/isUnique.java
@@ -1,1 +1,17 @@
-
+boolean isUnique(String s)  // Time: O(n) / n = len of s | Space: O(1)
+{
+    // assume character set is ASCII
+    if (s.length() > 128) return false;
+    
+    // flag at index i indicates whether char i in the alphabet is contained in string
+    boolean[] charSet = new boolean[128];
+    
+    for (int i=0; i<s.length(); i++)
+    {
+        int index = s.charAt(i);
+        if (charSet[index]) return false;  // already found this char
+        charSet[index] = true;
+    }
+    
+    return true;
+}
