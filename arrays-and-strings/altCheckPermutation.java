f71a28d485bf07e7f76b13bb731eb5334c22274c
Linh Do <linhdo@umass.edu>
2016-07-14 21:59:20 +0700

    Update altCheckPermutation.java

--- arrays-and-strings/altCheckPermutation.java
+++ arrays-and-strings/altCheckPermutation.java
@@ -1,1 +1,16 @@
-String 
+// 2 strings are permutations: same chars, but diff orders
+// sorting will put chars from 2 permutations in same order
+// just need to compare sorted versions
+
+String sort(String s)   // Time: O(n logn)
+{
+    char[] content = s.toCharArray();
+    java.util.Arrays.sort(content);
+    return new String(content);
+}
+
+boolean permutation(String s, String t)
+{
+    if (s.length() != t.length()) return false;
+    return sort(s).equals(sort(t));
+}
