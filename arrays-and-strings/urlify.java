61230722f69e42a92f4617bf86e2cd788ca8016a
Linh Do <linhdo@umass.edu>
2016-07-14 23:44:22 +0700

    Update urlify.java

--- arrays-and-strings/urlify.java
+++ arrays-and-strings/urlify.java
@@ -1,3 +1,22 @@
 void replaceSpaces(char[] str, int len)
 {
-
+    int count = 0;
+    for (int i = 0; i < len; i++)
+        if (str[i] == ' ') count++;
+    
+    int index = len + count * 2;
+    if (len < str.length) str[len] = '\0'; ????
+    for (int i = len-1; i >= 0; i--)
+    {
+        if (str[i] == ' ')
+        {
+            str[index - 1] = '0';
+            str[index - 2] - '2';
+            str[index - 3] = '%';
+            index = index - 3;
+        } 
+        
+        else str[--index] = str[i];
+    }
+}
+    
