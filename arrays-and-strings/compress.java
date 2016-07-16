5e59be80f2e766baca1aea3af4792ed716800d63
Linh Do <linhdo@umass.edu>
2016-07-16 12:39:08 +0700

    Update compress.java

--- arrays-and-strings/compress.java
+++ arrays-and-strings/compress.java
@@ -4,18 +4,15 @@ If the compressed string would not become smaller, return original str.
 The str has only uppercase and lower case letters (a - z).
 */
 
-String compress(String s)
-{
+String compress(String s) {
     // create compressed str
     StringBuilder result = new StringBuilder();
     int count = 0;
-    for (int i=0; i < s.length(); i++)
-    {
+    for (int i=0; i < s.length(); i++) {
         count++;
         
         // append this char: end of loop or next char is diff
-        if (i + 1 >= s.length() || s.charAt(i) != s.charAt(i+1)) 
-        {
+        if (i + 1 >= s.length() || s.charAt(i) != s.charAt(i+1)) {
             result.append(s.charAt(i));
             result.append(count);
             count = 0;
