907481caee40dbd2920ade30d7ab1970ef807bc7
Linh Do <linhdo@umass.edu>
2016-07-17 14:35:06 +0700

    Update removeDups.java

--- linked-lists/removeDups.java
+++ linked-lists/removeDups.java
@@ -1,1 +1,21 @@
-void delete
+/* 
+unsorted linked list
+Time:   O(n) / n elts 
+*/
+
+void deleteDups(Node n) {
+    HashSet<Integer> set = new HashSet<Integer>();
+    Node prev = null;
+    
+    // iterate thru linked list and add each elt to set
+    while (n != null) {
+        // if discover dup, remove elt and continue
+        if  (set.contains(n.data)) prev.next = n.next; 
+        else {
+            set.add(n.data);
+            prev = n;
+        }
+        n = n.next;
+    }
+}
+
