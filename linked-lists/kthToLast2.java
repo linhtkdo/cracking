fea533315f38ae54de6a49cc3d31de96e2a25c45
Linh Do <linhdo@umass.edu>
2016-07-18 20:58:10 +0700

    Update kthToLast2.java

--- linked-lists/kthToLast2.java
+++ linked-lists/kthToLast2.java
@@ -4,4 +4,20 @@ Iterative:
 Space: O(1) | Time: O(n)
 */
 
-Node kthToLast()
+Node kthToLast(Node head, int k) {
+    // Put p2 at the beginning
+    Node p1 = head;
+    Node p2 = head;
+    
+    // Move p1 k nodes into list 
+    for (int i = 0; i < k; i++) {
+        if (p1 == null) return null; // out of bounds
+        p1 = p1.next;
+    }
+    // p1 will hit end of list after (len-k) steps. p2 will be k nodes from end
+    while (p1 != null) {
+        p1 = p1.next;
+        p2 = p2.next;
+    }
+    return p2;
+}
