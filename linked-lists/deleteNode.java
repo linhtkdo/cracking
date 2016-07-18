e7fcd38636190f92dd4bed40211b6eee157b8abe
Linh Do <linhdo@umass.edu>
2016-07-18 21:55:50 +0700

    Update deleteNode.java

--- linked-lists/deleteNode.java
+++ linked-lists/deleteNode.java
@@ -1,1 +1,14 @@
+/*
+Delete a node in the middle.
+In:     node c from 1-->5-->9-->4
+Result:             1-->5-->4
+*/
+
+boolean deleteNode(Node n) {
+    if (n == null || n.next == null) return false;
+    Node next = n.next;
+    n.data = next.data; // copy data of next node to n
+    n.next = next.next; // delete next node
+    return true;
+}
 
