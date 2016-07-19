f68bcce0460e953eccf9c9da7c008baad3ded29a
Linh Do <linhdo@umass.edu>
2016-07-19 20:30:23 +0700

    Update sum.java

--- linked-lists/sum.java
+++ linked-lists/sum.java
@@ -9,12 +9,15 @@ Out:     3 -> 8 -> 5 -> 8
 Node add(l1, l2, int carry) {
     if (l1 == null && l2 == null && carry == 0) return null;
     
+    // add l1 node and l2 node (and carry)
     Node result = new Node();
     int val = carry;
     if (l1 != null) val += l1.data;
     if (l2 != null) val += l2.data;
     result.data = val % 10 // record second digit of number 
     
+    // recursive stack: node vals in sum list
+    // go back up: current.next = lower level
     if (l1 != null || l2 != null) {
         Node more = add(l1 == null ? null : l1.next,    // one list may be shorter than another
                         l2 == null ? null : l2.next,    // don't want null pointer exception
