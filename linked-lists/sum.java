ff6e6168fa0c1b70056e4bd044b0d506af446f05
Linh Do <linhdo@umass.edu>
2016-07-19 20:17:07 +0700

    Update sum.java

--- linked-lists/sum.java
+++ linked-lists/sum.java
@@ -1,1 +1,26 @@
+/*
+In:     (7 -> 1 -> 6) + (5 -> 9 -> 2)
+Out:     2 -> 1 -> 9
 
+In:     (1 -> 5 -> 9) + (2 -> 3 -> 6 -> 7)
+Out:     3 -> 8 -> 5 -> 8
+*/
+
+Node add(l1, l2, int carry) {
+    if (l1 == null && l2 == null && carry == 0) return null;
+    
+    Node result = new Node();
+    int val = carry;
+    if (l1 != null) val += l1.data;
+    if (l2 != null) val += l2.data;
+    
+    result.data = val % 10 // record second digit of number 
+    
+    if (l1 != null || l2 != null) {
+        Node more = add(l1 == null ? null : l1.next,    // one list may be shorter than another
+                        l2 == null ? null : l2.next,    // don't want null pointer exception
+                        val >= 10 ? 1 : 0);             // carry excess data to next node
+        result.setNext(more);
+    }
+    return result;
+}
