7f9a9bd6a45fa5f16ebd60665c7304473693e797
Linh Do <linhdo@umass.edu>
2016-07-21 00:02:15 +0700

    Update palindrome3.java

--- linked-lists/palindrome3.java
+++ linked-lists/palindrome3.java
@@ -1,1 +1,17 @@
+/* Recursive Approach */
+
+class Result {
+  public Node node;
+  public boolean result;
+}
+
+boolean isPalindrome(Node head) {
+  int len = length(head);
+  Result p = helper(head, len);
+  return p.result;
+}
+
+Result helper(Node head, int len) {
+  if (head == null ||)
+}
 
