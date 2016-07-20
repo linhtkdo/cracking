476364f190c9ceb9805b0f0d290c805fdd9c3269
Linh Do <linhdo@umass.edu>
2016-07-20 17:03:01 +0700

    Update palindrome.java

--- linked-lists/palindrome.java
+++ linked-lists/palindrome.java
@@ -1,1 +1,26 @@
+/* Reverse and Compare */
 
+boolean isPalindrome(Node head) {
+  Node rev = reverse(head);
+  return isEqual(head, rev);
+}
+
+Node reverse(Node node) {
+  Node head = null;
+  while (node != null) {
+    Node n = new Node(node.data); // clone
+    n.next = head;
+    head = n;
+    node = node.next;
+  }
+  return head;
+}
+
+boolean isEqual(Node one, Node two) {
+  while (one != null && two != null) {
+    if (one.data != two.data) return false;
+    one = one.next;
+    two = two.next;
+  }
+  return one == null && two == null;
+}
