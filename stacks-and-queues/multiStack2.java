b5786fc9d88d4501c2fb508c258163127db96933
Linh Do <linhdo@umass.edu>
2016-07-26 23:05:17 +0700

    Update multiStack2.java

--- stacks-and-queues/multiStack2.java
+++ stacks-and-queues/multiStack2.java
@@ -1,1 +1,27 @@
-
+public class MultiStack {
+  private class Info {
+    public int start, size, cap;
+    public Info(int start, int cap) {
+      this.start = start;
+      this.cap = cap;
+    }
+    
+    public boolean isWithinStackCap(int i) {
+      if (i < 0 || i >= vals.length) return false;
+      int contiguous = i < start ? i + vals.len : i;
+      int end = start + cap;
+      return start <= contiguous && contiguous < end;
+    } 
+    
+    public int lastCapIn() {
+      return adjustIn(start + cap - 1);
+    }
+    
+    public int lastEltIn() {
+      return adjustIn(start + size - 1);
+    }
+    
+    public boolean isFull() { return size == cap;}
+    public boolean isEmpty
+  }
+}
