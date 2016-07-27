e06ad4386dcfeeb03d0d10375ec135deb4babf25
Linh Do <linhdo@umass.edu>
2016-07-27 15:18:31 +0700

    Update stackMin.java

--- stacks-and-queues/stackMin.java
+++ stacks-and-queues/stackMin.java
@@ -1,1 +1,25 @@
+/* 
+Design a stack which has push, pop, and min that all operate in O(1) time. 
+Have each node record what the min beneath itself is. 
+*/
 
+public class StackWithMin extends Stack<Node> {
+  public void push(int val) {
+    int newMin = Math.min(val, min());
+    super.push(new NodeWithMin(val, newMin));
+  }
+  
+  public int min() {
+    if (this.isEmpty()) return Integer.MAX_VALUE; // error val
+    else return peek().min;
+  }
+}
+
+class NodeWithMin {
+  public int val;
+  public int min;
+  public NodeWithMin(int v, int min) {
+    val = v;
+    this.min = min;
+  }
+}
