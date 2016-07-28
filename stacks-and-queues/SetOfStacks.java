24ba77ff4e0a312f4a54ee2032a0915c290d863f
Linh Do <linhdo@umass.edu>
2016-07-28 12:10:13 +0700

    Update SetOfStacks.java

--- stacks-and-queues/SetOfStacks.java
+++ stacks-and-queues/SetOfStacks.java
@@ -1,6 +1,6 @@
 /*
 SetOfStacks creates a new stack once the prev one exceeds cap.
-Follow up:  popAt(int index) *later*.
+Follow up:  popAt(int index) on a specific substack.
 */
 
 import java.util.ArrayList;
@@ -38,6 +38,33 @@ class SetOfStacks {
     // if empty after pop: remove from list of stacks
     if (last.size == 0) stacks.remove(stacks.size() - 1);
     return v;
+  }
+  
+  public boolean isEmpty() {
+    Stack last = getLastStack();
+    return last == null || last.isEmpty();
+  }
+  
+  public int popAt(int i) {
+    return leftShift(i, true);
+  }
+  
+  // If we pop an elt from s1, we need to remove the bottom of s2 and push it onto s1.
+  // We then rollover from s3 to s2, etc.
+  public int leftShift(int i, boolean flag) {
+    Stack s = stacks.get(i);
+    
+    int x;
+    if (flag) x = s.pop();
+    else x = s.bottom();
+    
+    if (s.isEmpty()) stacks.remove(i);
+    else if (stacks.size() > i + 1) {   // end at last stack
+      int v = leftShift(i + 1, false);  // recurse to collect bottoms
+      s.push(v);                        // go up and push to curr stack
+    }
+    
+    return x;
   }
 }
 
