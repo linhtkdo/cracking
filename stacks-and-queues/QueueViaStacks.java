f142ede39b9241c9269a4883bfdebd42fa8a2d6e
Linh Do <linhdo@umass.edu>
2016-08-01 21:49:34 +0700

    Update QueueViaStacks.java

--- stacks-and-queues/QueueViaStacks.java
+++ stacks-and-queues/QueueViaStacks.java
@@ -13,4 +13,27 @@ public class MyQueue<T> {
   public int size() {
     return s1.size() + s2.size();
   }
+  
+  // push onto stackNewest
+  public void add(T val) {
+    s1.push(val);
+  }
+  
+  // use Stack API to check if stackOldest is empty; 
+  // then transfer all elts from stackNewest into stackOldest 
+  private void shiftStacks() {
+    if (s2.isEmpty())
+      while (!s1.isEmpty()) s2.push(s1.pop());
+  }
+  
+  // dequeue from stackOldest
+  public T peek() {
+    shiftStacks(); // ensure s1 has the curr elts
+    return s2.peek();
+  }
+  
+  public T remove() {
+    shiftStacks();
+    return s2.pop();
+  }
 }
