807b861d7d0a9f05578db93a0dd701183707e5ca
Linh Do <linhdo@umass.edu>
2016-08-01 14:22:52 +0700

    Update QueueViaStacks.java

--- stacks-and-queues/QueueViaStacks.java
+++ stacks-and-queues/QueueViaStacks.java
@@ -1,2 +1,16 @@
 /*
-Implement a queue using 2 
+Implement a queue using 2 stacks.
+*/
+
+public class MyQueue<T> {
+  Stack<T> s1, s2;
+  
+  public MyQueue() {
+    s1 = new Stack<T>();
+    s2 = new Stack<T>();
+  }
+  
+  public int size() {
+    return s1.size() + s2.size();
+  }
+}
