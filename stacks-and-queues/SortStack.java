40e8f0e846a65a0114b7a77041cfe5f59ab09a65
Linh Do <linhdo@umass.edu>
2016-08-02 21:45:53 +0700

    Update SortStack.java

--- stacks-and-queues/SortStack.java
+++ stacks-and-queues/SortStack.java
@@ -5,7 +5,6 @@ Can use an additional temporary stack, but may not copy the elts into any other 
 2. Use isEmpty before any pop/ peek.
 3. Use 2 stacks to reverse order (biggest vs smallest/ oldest vs newest on top).
 4. Look out for redundant code (don't need code for push back the bigger elts: still pop from s and push to r).
-
 */
 
 void sort (Stack<Integer> s) {
