e9ef4c6016c12ce96ca2c838068a298df6633916
Linh Do <linhdo@umass.edu>
2016-08-05 21:24:45 +0700

    Update Route.java

--- trees-and-graphs/Route.java
+++ trees-and-graphs/Route.java
@@ -6,27 +6,27 @@ Given a directed graph, find out whether there is a route btw 2 nodes.
 4. Special case (start==end) -> terminate at start. 
 */
 
-enum State { Unvisited, Visited, Visiting; }
+enum State { Unvisited, Visited, Visiting; } // mark
 
 boolean search(Graph g, Node start, Node end) {
   if (start == end) return true;
-  
   LinkedList<Node> q = new LinkedList<Node>();
-  
   for (Node u : g.getNodes()) u.state = State.Unvisited;
+  
   start.state = State.Visiting;
   q.add(start);
-  Node u;
+  
   while (!q.isEmpty()) {
-    u = q.removeFirst();
-    for (Node v : u.getAdjacent()) 
+    Node u = q.removeFirst();               // dequeue
+    for (Node v : u.getAdjacent())          // add and mark all adj
       if (v.state == State.Unvisited) {
-        if (v == end) return true;
+        if (v == end) return true;          // check
         v.state = State.Visiting;
         q.add(v);
       }
     u.state = State.Visited;
   }
+  
   return false;
 }
 
