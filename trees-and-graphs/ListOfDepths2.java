05a0df2d47dfcbdc49eca4109f2ce530917d9be4
Linh Do <linhdo@umass.edu>
2016-08-08 23:33:15 +0700

    Update ListOfDepths2.java

--- trees-and-graphs/ListOfDepths2.java
+++ trees-and-graphs/ListOfDepths2.java
@@ -1,1 +1,20 @@
-/*
+/* 
+Given a binary tree, create a linked list of all the nodes at each depth.
+If you have a tree with depth D, you'll have D linked lists.
+
+1. bfs
+*/
+
+ArrayList<LinkedList<Node>> getList(Node root) {
+  ArrayList<LinkedList<Node>> res = new ArrayList<LinkedList<Node>>();
+  LinkedList<Node> cur = new LinkedList<Node>();
+  if (root != null) cur.add(root);
+  
+  while (cur.size() > 0) {
+    res.add(cur); 
+    LinkedList<Node> parents = cur;
+    cur = new LinkedList<Node>();
+    
+    for 
+  }
+}
